04321b9838a9201aad61bd1e47906ca6255efd56
vishal-dembla <demblasdesk@gmail.com>
2025-10-02 16:04:18 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+let deals = [
+  {
+    startup: "Spenny",
+    acquiredBy: "Cred",
+    industry: "Fintech, Micro-investment",
+    employees: "Not specified",
+    info: "YCombinator-backed, nudged micro-investments",
+    date: "2023-06-22",
+    source: "inc42"
+  },
+  {
+    startup: "Travenues",
+    acquiredBy: "SpiceJet",
+    industry: "Airline E-commerce Technology",
+    employees: "Not specified",
+    info: "Strengthens SpiceJet's e-commerce technology",
+    date: "",
+    source: "Economic Times"
+  },
+  {
+    startup: "Cathyos Labs",
+    acquiredBy: "Automation Anywhere",
+    industry: "Product Engineering",
+    employees: "Not specified",
+    info: "Boosts engineering for product development",
+    date: "",
+    source: "Economic Times"
+  },
+  {
+    startup: "Pikup.ai",
+    acquiredBy: "Ola",
+    industry: "AI, Autonomous Mobility Tech",
+    employees: "Not specified",
+    info: "Specializes in AI, computer vision, sensor fusion",
+    date: "",
+    source: "Economic Times"
+  }
+];
